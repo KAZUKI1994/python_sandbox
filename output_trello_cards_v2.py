3cb49eb4acf5622e49770dbd6ed37f00e29e18a0
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2017-09-20 18:43:28 +0900

    アーカイブしているカードを抽出対象から除外

--- output_trello_cards_v2.py
+++ output_trello_cards_v2.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 import json
 
-f = open('filename.json', 'r')
+# set json file exported from trello at current directory
+f = open('file.json', 'r')
 data = json.load(f)
 
 # 
@@ -28,22 +29,24 @@ for list in data['lists']:
 counter = 1
 print "項番, ステータス, カード名, 説明, 担当者, 更新日時"
 for card in data['cards']:
-	updated_date = card['dateLastActivity']
-	desc = card['desc']
-	card_id = card['id']
-	title = card['name']
-	asigned_member_ids = card['idMembers']
-	list_id = card['idList']
+	closed = card['closed']
+	if (closed == True):
+		updated_date = card['dateLastActivity']
+		desc = card['desc']
+		card_id = card['id']
+		title = card['name']
+		asigned_member_ids = card['idMembers']
+		list_id = card['idList']
 
-	# asigned_member_name
-	if len(asigned_member_ids) > 0:
-		asigned_member_id = asigned_member_ids[0]
-		asigned_member_name = members[asigned_member_id]
-	else:
-		asigned_member_name = ""
+		# asigned_member_name
+		if len(asigned_member_ids) > 0:
+			asigned_member_id = asigned_member_ids[0]
+			asigned_member_name = members[asigned_member_id]
+		else:
+			asigned_member_name = ""
 
-	# list_name
-	list_name = lists[list_id]
+		# list_name
+		list_name = lists[list_id]
 
-	print str(counter) + " ," + list_name + "," + title + " ," + desc + " ," + asigned_member_name + "," + updated_date
-	counter = counter + 1
+		print str(counter) + ", " + list_name + ", " + title + ", " + desc + ", " + asigned_member_name + ", " + updated_date
+		counter = counter + 1
