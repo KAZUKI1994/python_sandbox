b0eccbb8d4166952ed47c38f6ad605adfab70a83
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2017-09-20 15:51:07 +0900

    trelloのjsonからcsv形式のテキストを標準出力する。

---
+++ output_trello_cards_v2.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+import json
+
+f = open('filename.json', 'r')
+data = json.load(f)
+
+# 
+# getting members
+# 
+members = {}
+for member in data['members']:
+	member_id = member['id']
+	member_name = member['fullName']
+	members.update({member_id: member_name})
+
+#
+# getting lists
+#
+lists = {}
+for list in data['lists']:
+	list_id = list['id']
+	list_name = list['name']
+	lists.update({list_id: list_name})
+
+#
+#  getting cards
+#  
+counter = 1
+print "項番, ステータス, カード名, 説明, 担当者, 更新日時"
+for card in data['cards']:
+	updated_date = card['dateLastActivity']
+	desc = card['desc']
+	card_id = card['id']
+	title = card['name']
+	asigned_member_ids = card['idMembers']
+	list_id = card['idList']
+
+	# asigned_member_name
+	if len(asigned_member_ids) > 0:
+		asigned_member_id = asigned_member_ids[0]
+		asigned_member_name = members[asigned_member_id]
+	else:
+		asigned_member_name = ""
+
+	# list_name
+	list_name = lists[list_id]
+
+	print str(counter) + " ," + list_name + "," + title + " ," + desc + " ," + asigned_member_name + "," + updated_date
+	counter = counter + 1
